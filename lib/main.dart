cecff53e96290949c9562ccfe9cc3ca0a4bfb813
Thein Zan <theinzan@metateammyanmar.com>
2025-02-04 14:25:33 +0630

    Refactor main.dart to update greeting display and remove test.js file

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Text("Hello, greeting to all users."),
+              ),
+              Container(
+                child: Text("Hello, greeting to all users."),
+              ),
+            ],
+          ),
         ),
       ),
     );
